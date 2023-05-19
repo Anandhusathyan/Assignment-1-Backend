59ff170c24f4c67e204a6ccc9a28d99f2fed5ce0
Anandhusathyan <sathyananandhu1@gmail.com>
2023-05-19 12:00:59 +0530

    my first commit

---
+++ src/api/game/controllers/custom.js
@@ -0,0 +1,13 @@
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::game.game', ({ strapi }) =>  ({
+  // Method 1: Creating an entirely custom action
+  async exampleAction(ctx) {
+    try {
+      ctx.body = 'ok';
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}))
+ 
